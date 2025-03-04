d05e951d50c4f52f217e516c156742c59484498f
Boran Henden <89026715+henden-b@users.noreply.github.com>
2025-03-04 18:02:30 +0100

    0.0.3 small but steady process

--- V1/QPU_V1/QC_V1.py
+++ V1/QPU_V1/QC_V1.py
@@ -4,4 +4,20 @@ from imageloader_V1 import getImageandPad
 import numpy as np
 import matplotlib.pyplot as plt
 
-qc = QuantumCircuit(10)
+normalized_Image = getImageandPad().flatten()
+
+image_Vector = normalized_Image / np.linalg.norm(normalized_Image)
+
+num_qubits = int(np.ceil(np.log2(len(image_Vector))))
+num_elemnts_req = 2**num_qubits
+
+# Vektor auf die nächste Potenz von 2 auffüllen
+padded_vector = np.zeros(num_elemnts_req)
+padded_vector[:len(image_Vector)] = image_Vector
+
+print("We require this number of qubits: ", num_qubits)
+qc = QuantumCircuit(num_qubits)
+
+init_gate = qc.initialize(padded_vector, range(num_qubits))
+init_gate.label = 'init'
+print(qc.decompose().draw('text'))
--- V1/QPU_V1/imageloader_V1.py
+++ V1/QPU_V1/imageloader_V1.py
@@ -2,7 +2,7 @@ import tensorflow.keras as ks
 import numpy as np
 import matplotlib.pyplot as plt
 
-def getImageandPad():
+def getImageandPad(show_image=False):
     print('Process Started') #Start Meldung
 
     #Load important stuff
@@ -14,13 +14,18 @@ def getImageandPad():
     print('currently working on a image like: ', label)
 
     padded_image = np.pad(image, pad_width=4, mode='constant', constant_values=0)
-    # Bild anzeigen
-    plt.figure(figsize=(2, 2))
-    plt.imshow(padded_image, cmap='gray')
-    plt.title(f'Label: {label}')
-    plt.axis('off')
-    plt.show()
+    normalized_image = padded_image / 255.0
+
+    # Bild anzeigen nur wenn true
+    if(show_image == True):
+        plt.figure(figsize=(2, 2))
+        plt.imshow(padded_image, cmap='gray')
+        plt.title(f'Label: {label}')
+        plt.axis('off')
+        plt.show()
+
+    return normalized_image
 
 if(__name__ == '__main__'):
-    getImageandPad()
+    getImageandPad(True)
 
